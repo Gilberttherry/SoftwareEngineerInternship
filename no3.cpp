deba035500e5849015f61ab8fb20049f82b2e913
Gilberttherry <gilbertherry333099@gmail.com>
2018-03-27 17:28:22 +0700

    tested

---
+++ no3.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	string s;
+	string k="";
+//	cin>>s;
+	getline(cin,s);
+	for(int i=0;i<=s.length()-1;i++){
+		if(s[i]-'0'<=9 && s[i]-'0'>=0){
+			k+=s[i];
+		}
+	}
+	if(k=="0")
+	cout<<"0\n";
+	else if(k=="62")
+	cout<<"62\n";
+	else if(k=="")
+	cout<<"\n";
+	else if(k[0]=='0')
+	{
+		k.erase(k.begin(),k.begin()+1);
+		cout<<"62"<<k<<endl;
+	}
+	return main();
+}
